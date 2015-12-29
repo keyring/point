671ba5103fdac7ccb80b1dddf060d4d63fc9a563
keyring <keyrings@163.com>
2015-12-29 18:05:30 +0800

    not good

--- point.c
+++ point.c
@@ -32,31 +32,67 @@ void line_dda(struct point start, struct point end, struct color line_color, str
         x += xi;
         y += yi;
     }
-    
 }
 
 void line_midpoint(struct point start ,struct point end, struct color line_color, struct paint *painter)
 {
     int dx, dy, d, d1, d2, x, y;
-    dy = end.y - start.y;
-    dx = end.x - start.x;
-    d = 2 * dy + dx;
-    d1 = 2 * dy;
-    d2 = 2 * (dy + dx);
+    int a = start.y - end.y;
+    int b = end.x - start.x;
     x = start.x;
     y = start.y;
 
+    dx = end.x - start.x;
+    dy = end.y - start.y;
+    int xi = dx < 0 ? -1 : 1;
+    int yi = dy < 0 ? -1 : 1;
+    int steps = abs(dx) >= abs(dy) ? abs(dx) : abs(dy);
+
     if(abs(dx) > abs(dy)){
-        while(x <= end.x){
-            pixel(x, y, line_color, painter);
-            if(d < 0){ x++; y++; d+=d2; }
-            else{ x++; d+=d1; }
+        if(dy >= 0){
+            d =  2 * a + b;
+            d1 = 2 * (a);
+            d2 = 2 * (a) + 2 * b;
+            while(steps-- >= 0){
+                printf("%d,%d\n",x,y);
+                pixel(x, y, line_color, painter);
+                x+=xi;
+                if(d < 0){ y+=yi; d+=d2; }
+                else{ d+=d1; }
+            }
+        }else{
+            d = 2 * (xi*a) - b;
+            d1 = 2 * (xi*a) - 2*b;
+            d2 = 2 * (xi*a);
+            while(steps-- >= 0){
+                pixel(x, y, line_color, painter);
+                x+=xi;
+                if(d < 0){ d+=d2; }
+                else{ y+=yi; d+=d1; }
+            }
         }
+
     }else{
-        while(y <= end.y){
-            pixel(x, y, line_color, painter);
-            if(d < 0){ y++; x++; d+=d2; }
-            else{ y++; d+=d1; }
+        if(dx >= 0){
+            d = 2 * (yi*b) + a;
+            d1 = 2 * (yi*b);
+            d2 = 2 * (yi*b) + 2*a;
+            while(steps-- >= 0){
+                pixel(x, y, line_color, painter);
+                y+=yi;
+                if(d < 0){ x+=xi; d+=d2; }
+                else{ d+=d1; }
+            }
+        }else{
+            d = 2 * (yi*b) - a;
+            d1 = 2 * (yi*b) - 2*a;
+            d2 = 2 * (yi*b);
+            while(steps-- >= 0){
+                pixel(x, y, line_color, painter);
+                y+=yi;
+                if(d < 0){ d+=d2; }
+                else{ x+=xi; d+=d1; }
+            }
         }
     }
 
@@ -66,10 +102,10 @@ void line_midpoint(struct point start ,struct point end, struct color line_color
 void line(struct point start, struct point end, struct color line_color, struct paint *painter)
 {
     /* dda */
-    line_dda(start, end, line_color, painter);
+//    line_dda(start, end, line_color, painter);
 
     /* mid point  */
-    //line_midpoint(start, end, line_color, painter);
+    line_midpoint(start, end, line_color, painter);
     /* bresenham   */
 }
 
@@ -91,14 +127,14 @@ void render(struct paint *painter)
     pixel(40,40, color, painter);
 
     /* paint a line */
-    struct color line_color = {255, 0, 0};
+    struct color line_color = {0, 0, 0};
     struct point start = {50,50};
 
-    for(int i = 0; i < 100; i++){
-        int x = rand()%100;
-        int y = rand()%100;
+    for(int i = 0; i < 1; i++){
+        int x = 20;//rand()%100;
+        int y = 60;//rand()%100;
         struct point end = {x, y};
-        line(end, start, line_color, painter);
+        line(start, end, line_color, painter);
     }
 }
 
