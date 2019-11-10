37ba0c6cdd0523e6462be47a0050cffa191fca37
keyring <keyrings@163.com>
2019-11-11 00:12:28 +0800

    remove useless code

--- 3rd/ply-1.1/plyfile.c
+++ 3rd/ply-1.1/plyfile.c
@@ -191,9 +191,7 @@ PlyFile *ply_open_for_writing(
   float *version
 )
 {
-  int i;
   PlyFile *plyfile;
-  PlyElement *elem;
   char *name;
   FILE *fp;
 
@@ -395,9 +393,7 @@ void ply_element_count(
   int nelems
 )
 {
-  int i;
   PlyElement *elem;
-  PlyProperty *prop;
 
   /* look for appropriate element */
   elem = find_element (plyfile, elem_name);
@@ -517,7 +513,7 @@ Entry:
 
 void ply_put_element(PlyFile *plyfile, void *elem_ptr)
 {
-  int i,j,k;
+  int j,k;
   FILE *fp = plyfile->fp;
   PlyElement *elem;
   PlyProperty *prop;
@@ -1215,7 +1211,6 @@ PlyOtherElems *ply_get_other_element (
   PlyElement *elem;
   PlyOtherElems *other_elems;
   OtherElem *other;
-  int num_elems;
 
   /* look for appropriate element */
   elem = find_element (plyfile, elem_name);
@@ -1397,7 +1392,6 @@ Compare two strings.  Returns 1 if they are the same, 0 if not.
 
 int equal_strings(char *s1, char *s2)
 {
-  int i;
 
   while (*s1 && *s2)
     if (*s1++ != *s2++)
@@ -1470,7 +1464,7 @@ Entry:
 
 void ascii_get_element(PlyFile *plyfile, char *elem_ptr)
 {
-  int i,j,k;
+  int j,k;
   PlyElement *elem;
   PlyProperty *prop;
   char **words;
@@ -1592,7 +1586,7 @@ Entry:
 
 void binary_get_element(PlyFile *plyfile, char *elem_ptr)
 {
-  int i,j,k;
+  int j,k;
   PlyElement *elem;
   PlyProperty *prop;
   FILE *fp = plyfile->fp;
@@ -1736,7 +1730,6 @@ Exit:
 char **get_words(FILE *fp, int *nwords, char **orig_line)
 {
 #define BIG_STRING 4096
-  int i,j;
   static char str[BIG_STRING];
   static char str_copy[BIG_STRING];
   char **words;
